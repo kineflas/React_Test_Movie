011fdea0e17ad80a8698323c39a0341c07c87941
Kineflas <kineflas@student.42.fr>
2018-12-02 19:33:53 +0100

    Start tests

---
+++ src/components/MoviePreview/tests/index.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { TouchableOpacity } from 'react-native';
+
+import MoviePreview from '../index';
+import { Container, Content } from '../styles';
+import { WIN_WIDTH } from '../../../constants/dimensions';
+
+configure({ adapter: new Adapter() });
+
+describe('MoviePreview:', () => {
+    describe('index.js:', () => {
+        const props = {
+            movie: {
+                item: {
+                    poster_path: 'fakePosterPath'
+                }
+            },
+            baseUrl: 'fakeBaseUrl',
+            handleLoadMovie: () => {}
+        }
+        const wrapper = shallow(<MoviePreview {...props}/>);
+        it('ShouldFind a single TouchableOpacity', () => {
+            expect(wrapper.find(TouchableOpacity).length).toBe(1);
+        });
+        it('ShouldFind a single Container', () => {
+            expect(wrapper.find(Container).length).toBe(1);
+        });
+        it('ShouldFind a single Content', () => {
+            expect(wrapper.find(Content).length).toBe(1);
+        });
+    });
+});
